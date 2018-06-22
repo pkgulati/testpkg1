4f59767d89e1da982f9e55a9bcaa590c7ed781fc
Praveen Kumar Gulati <pkgulati@gmail.com>
2018-06-22 15:59:42 +0530

    added json data files

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ var util = require('./lib/util');
 console.log('testpkg1');
 
 exports.func1 = func1;
+exports.func2 = func2;
 
 function func1() {
-	console.log('func1');
-	util.listdir('dir1');
+	return util.listdir('dir1');
 }
 
 function func2() {
-	console.log('func2');
-	util.listdir('dir2');
+	return util.listdir('dir2');
 }
 
